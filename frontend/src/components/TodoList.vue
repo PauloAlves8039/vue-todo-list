6fc4679ca8341a3699bf16d47a2cd9e5f0c2ccb4
PauloAlves8039 <pj38alves@gmail.com>
2023-01-24 20:14:20 -0300

    Definindo integração com o backend

--- frontend/src/components/TodoList.vue
+++ frontend/src/components/TodoList.vue
@@ -5,9 +5,24 @@
 </template>
 
 <script>
+import axios from "axios";
+const baseURL = "http://localhost:3001/todos";
+
 export default {
   name: 'TodoList',
-  
+  data () {
+    return {
+      todos: [],
+    }
+  },
+  async created() {
+    try {
+      const response = await axios.get(baseURL);
+      this.todos = response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
 </script>
 
