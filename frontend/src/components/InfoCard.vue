a681c34eb43ef86808e1a5b64b3250b5d1e39947
PauloAlves8039 <pj38alves@gmail.com>
2023-01-29 12:56:44 -0300

    Adicionando componente para exibir informações

---
+++ frontend/src/components/InfoCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card" style="width: 18rem">
+    <div class="card-body">
+      <h5 class="card-title">Total de tarefas</h5>
+      {{ countTodo }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    countTodo: [],
+  },
+};
+</script>
+
+<style scoped>
+</style>
