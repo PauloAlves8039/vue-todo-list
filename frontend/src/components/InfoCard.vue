50e81aa27c9d526d76c6c7e94e68e2bac21c411b
PauloAlves8039 <pj38alves@gmail.com>
2023-05-16 12:13:04 -0300

    Criando o contador de tarefas

--- frontend/src/components/InfoCard.vue
+++ frontend/src/components/InfoCard.vue
@@ -1,19 +1,46 @@
 <template>
-  <div class="card" style="width: 18rem">
-    <div class="card-body">
-      <h5 class="card-title">Total de tarefas</h5>
-      {{ countTodo }}
+  <div class="container-card row">
+    <div class="card text-dark mb-3 col-8 card-total">
+        <div class="card-body">
+            <p class="card-text">
+                Total de Tarefas:
+                <span class="text-total">{{ countTodo }}</span>
+            </p>
+        </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'InfoCard',
   props: {
-    countTodo: [],
+    countTodo: {
+      type: Number,
+      required: true
+    }
   },
 };
 </script>
 
 <style scoped>
+.container-card {
+    display: flex;
+    justify-content: center;
+}
+.card-total {
+    background-color: #212529;
+    max-width: 16rem;
+    max-height: 3rem;
+}
+.card-text {
+    color: #FFF;
+    font-size: 18px;
+    font-weight: 400;
+    text-align: center;
+    margin-top: -5px;
+}
+.text-total {
+    font-weight: 800;
+}
 </style>
